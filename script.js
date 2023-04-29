a59f9855c1ec6147966ca52b030359e910a12bec
Eldad Chidra <eldadchb@gmail.com>
2023-04-30 00:09:53 +0200

    fixed: score >= 0

--- script.js
+++ script.js
@@ -1,54 +1,58 @@
-const flagImg = document.getElementById('flag-img');
-const quizForm = document.getElementById('quiz-form');
-const answerInput = document.getElementById('answer');
-const resultParagraph = document.getElementById('result');
-const scoreParagraph = document.getElementById('score');
-const scoreValue = document.getElementById('score-value');
+const flagImg = document.getElementById("flag-img");
+const quizForm = document.getElementById("quiz-form");
+const answerInput = document.getElementById("answer");
+const resultParagraph = document.getElementById("result");
+const scoreParagraph = document.getElementById("score");
+const scoreValue = document.getElementById("score-value");
 
 let flags = [];
 let currentFlag = {};
 let score = 0;
 
 async function fetchFlags() {
-    try {
-        const response = await fetch('https://restcountries.com/v3.1/all');
-        const data = await response.json();
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const data = await response.json();
 
-        flags = data.map(country => ({
-            country: country.name.common,
-            imageUrl: country.flags.png,
-        }));
-        displayFlag();
-    } catch (error) {
-        console.error('Error fetching flags:', error);
-    }
+    flags = data.map((country) => ({
+      country: country.name.common,
+      imageUrl: country.flags.png,
+    }));
+    displayFlag();
+  } catch (error) {
+    console.error("Error fetching flags:", error);
+  }
 }
 
 function getRandomFlag() {
-    const randomIndex = Math.floor(Math.random() * flags.length);
-    return flags[randomIndex];
+  const randomIndex = Math.floor(Math.random() * flags.length);
+  return flags[randomIndex];
 }
 
 function displayFlag() {
-    currentFlag = getRandomFlag();
-    flagImg.src = currentFlag.imageUrl;
+  currentFlag = getRandomFlag();
+  flagImg.src = currentFlag.imageUrl;
 }
 
 function checkAnswer(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    if (answerInput.value.trim().toLowerCase() === currentFlag.country.toLowerCase()) {
-        resultParagraph.textContent = 'Correct! Good job!';
-        score++;
-    } else {
-        resultParagraph.textContent = `Wrong. The correct answer is ${currentFlag.country}.`;
-        score--;
+  if (
+    answerInput.value.trim().toLowerCase() === currentFlag.country.toLowerCase()
+  ) {
+    resultParagraph.textContent = "Correct! Good job!";
+    score++;
+  } else {
+    resultParagraph.textContent = `Wrong. The correct answer is ${currentFlag.country}.`;
+    if (score > 0) {
+      score--;
     }
+  }
 
-    scoreValue.textContent = score;
-    answerInput.value = '';
-    displayFlag();
+  scoreValue.textContent = score;
+  answerInput.value = "";
+  displayFlag();
 }
 
-quizForm.addEventListener('submit', checkAnswer);
+quizForm.addEventListener("submit", checkAnswer);
 fetchFlags();
